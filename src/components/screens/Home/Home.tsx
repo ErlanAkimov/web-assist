2ef7f94b3c04ebbb9e7315c410721294225b3706
erlan_akimov <erlan_akimov_dev@mail.ru>
2023-09-24 00:56:53 +0500

    Восстановил рендер PostCard'ов для главной

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,22 +1,35 @@
+import Donate from '@/components/Donate/Donate';
 import styles from './home.module.scss';
 import Layout from '@/components/layout/layout';
 import PostCard from '@/components/PostCard/PostCard';
+import ProfileCard from '@/components/ProfileCard/ProfileCard';
 import { IPostData } from '@/interfaces/post.interface';
-import React, { FC } from 'react';
+import { FC } from 'react';
 
-const Home: FC<IPostData> = ({ posts }) => {
+const Home: FC<IPostData> = ({posts}) => {
+
 	return (
 		<Layout
 			title="Главная"
 			description="web-assist лучшая библиотека для начинающих разработчиков."
 		>
-			<div className="postline">
-				{
-					console.log(posts)
-				}
+			<div className={styles.local_wrapper}>
+				<div className="postline">
+					{
+						posts.map((post) => {
+							return (<PostCard key={post.post_id} post={post} />);
+						})
+					}
+
+				</div>
+
+				<div>
+					<ProfileCard />
+					<Donate />
+				</div>
 			</div>
 		</Layout>
 	);
-};
+}
 
-export default Home;
+export default Home;	
