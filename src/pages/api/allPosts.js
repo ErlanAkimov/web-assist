099ee9d29edb731402a01759cb7f0556a074ad6d
erlan_akimov <erlan_akimov_dev@mail.ru>
2023-10-21 16:53:12 +0500

    try to connect

--- src/pages/api/allPosts.js
+++ src/pages/api/allPosts.js
@@ -2,10 +2,11 @@ import pool from './db'; // Импортируем пул подключений
 
 export default async (req, res) => {
   try {
-    const [rows, fields] = await pool.promise().query('SELECT * FROM posts');
-    res.status(200).json({ data: rows });
+    const rows = await pool.promise().query('SELECT * FROM posts');
+    res.status(200).json({ data: 'asf' });
+
   } catch (error) {
     console.error('Ошибка при выполнении запроса:', error);
-    res.status(500).send('500');
+    res.status(500).send(error);
   }
 };
--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,26 +1,10 @@
 // dbConfig.js
 import mysql from 'mysql2';
 
-// const connection = mysql.createConnection({
-//   host: '5.23.50.27',
-//   user: 'pilearl_wa',
-//   password: 'Trader16',
-//   database: 'pilearl_wa'
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     return console.error(err.message)
-//   } else {
-//     console.log('norm')
-//   }
-// })
-
-// connection.connect 
-
 const dbConfig = {
+  connectionLimit: 10,
   host: 'vh424.timeweb.ru',
-  user: 'pilearl_wa',
+  user: 'pilearl',
   password: 'Trader16',
   database: 'pilearl_wa'
 };
